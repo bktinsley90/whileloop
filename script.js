8626f361babd24d4c2aaa47e00f0c68aba5e226a
Brittany Tinsley <brittanytinsley@Brittanys-MBP.jetpack>
2020-03-25 13:38:43 -0400

    Completed while loop assignment

---
+++ script.js
@@ -0,0 +1,36 @@
+// let i = 0;
+// Write a loop that will console.log "I am starting to feel a little loopy" 3 times.
+// while (i <= 2 ){
+//     console.log("I am starting to feel loopy");
+//     i++;
+// }
+// Write a while loop that will iterate from 0 to 20.
+// while(i <= 20) {
+//     if (i % 2){
+//         console.log(`${i} is Even`);
+//     }else {
+//         console.log(`${i} is Odd`);
+//     } i++;        
+// }
+// Write a while loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18")
+// while(i <=10 ) {
+//     let result = i * 9;
+//     console.log( `${i} * 9 = ${result}`);
+//     i++
+// } 
+// Make a while loop that has a variable with a value of 36. As you loop down to 1, return the sum of when you divide the variable (at its current state in that iteration) by 2
+
+// let i = 36;
+// while (i >= 1) {
+//     console.log(i / 2 );
+//     i--;
+// }
+
+
+// let i = 0 
+// while (i <= 80) {
+//     console.log (`There is room for only ${80 - i }  of books `)
+//     i++;
+// }
+
+
